bd1454b0dc6deb4923dba9ed1f15e526c4e8ed26
Bernardo Taveira <bertaveira@localhost.localdomain>
2018-11-13 12:50:14 +0000

    Start Point

--- main.c
+++ main.c
@@ -1,1 +1,31 @@
-test
+/******************************************************************************
+ *
+ * File Name: main.c
+ *
+ * NAME
+ *     main
+ *
+ * SYNOPSIS
+ *		#include <stdlib.h>
+ *		#include <stdio.h>
+ *		#include <string.h>
+ *		#include "word.h"
+ *
+ * DESCRIPTION
+ *
+ * DIAGNOSTICS
+ *          tested
+ *****************************************************************************/
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+#include "map.h"
+
+
+int main(int argc, char **argv) {
+  
+}
---
+++ map.h
@@ -0,0 +1,9 @@
+#ifndef _MAP_H_
+#define _MAP_H_
+
+#include "defs.h"
+
+
+
+
+#endif
