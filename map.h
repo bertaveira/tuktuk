8fa88169d958e5035959e14b5dc8b431253657ad
bertaveira <bertaveira@gmail.com>
2018-11-30 16:16:22 +0000

    missing functions

--- map.h
+++ map.h
@@ -9,6 +9,8 @@ typedef struct _node node;
 
 map* readMap(FILE * fp);
 short int inMapCheck (map *mp, short int x, short int y);
+char getMode(map * mp);
+short int getPOI(map * mp, short int a, short int b);
 void modeA(map *mp, FILE *fpw);
 void clearList(list *lt);
 void printPoints(list *lt, FILE *fpw, int *count);
@@ -20,6 +22,7 @@ short int getX(Item a);
 short int getY(Item a);
 void freeMap(map *mp);
 void printerror(map * mp, FILE *fpw);
+
 
 
 #endif
