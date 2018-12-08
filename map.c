21ed1283a35a0ce813673b55e93bbac92eb358f6
bertaveira <bertaveira@gmail.com>
2018-12-08 01:17:11 +0000

    Comments

--- map.c
+++ map.c
@@ -147,60 +147,93 @@ void modeB(map *mp, FILE *fpw){
 
 
 
-
+/**
+*  - modeC - Run variable C of the program
+*
+* @param map *mp - structure with map of cost, size of board and points of interest
+* @param FILE *fpw - file to output to
+*
+**/
 void modeC(map *mp, FILE *fpw){
-  list ***adj = (list ***)malloc(sizeof(list **)*mp->nPoints);
+  list ***adj = (list ***)malloc(sizeof(list **)*mp->nPoints); // matrix of paths between two points
   list *lt = NULL;
   int i, j, cost = 0, count = 0;
-  int best[mp->nPoints], vect[mp->nPoints];
+  int best[mp->nPoints], vect[mp->nPoints]; // best path and path being examined
 
+  // each list stores the best path between two points (adj[a][b] - path a->b)
   for(i=0; i<mp->nPoints; i++) adj[i] = (list **)malloc(sizeof(list *)*mp->nPoints);
-
   for( i = 0; i<mp->nPoints; i++) {
     adj[i][i] = NULL;
     for (j = i+1; j<mp->nPoints; j++) {
-      adj[i][j] = shortestPath(mp, i, j, 0);
+      adj[i][j] = shortestPath(mp, i, j, 0); // get best path between points i and j (from mp->points[][x])
       freeHeap();
-      clearList(adj[i][j]);
+      clearList(adj[i][j]); // remove from list the points no in best path
+      // backwords path set to NULL (later if needed we can reverse the list)
       adj[j][i] = NULL;
     }
   }
-  vect[0] = 0;
-
+  //start point is 0
+  vect[0] = 0; // each number "x" coresponds to the point (mp->points[1][x], mp->points[0][x])
+  // call recursive function to find best hamilton path between the points of interest
   hamAndCheese(1, mp, adj, vect, best, 0, &cost);
   if(DEBUG) printf("A very nice ham indeed\n");
+
+  // no possible path
   if( cost == 0 ) {
     printerror(mp, fpw);
     freeAdj(adj, mp->nPoints);
     return;
   }
   if(DEBUG) printf("melt that cheese\n");
+
+  // merge all the lists of paths between points to form the best overall path
   for (i = 0; i<mp->nPoints-1; i++) {
+    // list backwords... need to reverse it
     if (adj[vect[i]][vect[i+1]] == NULL) {
       adj[vect[i]][vect[i+1]] = reverseList(adj[vect[i+1]][vect[i]], mp, vect[i+1]);
-      adj[vect[i+1]][vect[i]] = NULL;
+      adj[vect[i+1]][vect[i]] = NULL; // prevent future errors by assaining null
     }
+    // transfer list to the mega list
     lt = (list *)mergeLists(adj[vect[i]][vect[i+1]], lt);
-    adj[vect[i]][vect[i+1]] = NULL;
   }
   nullCheck(lt);
+
+  //remove vertices from the adj matrix to prevent double frees
+  for (i = 1; i<mp->nPoints; i++) {
+    adj[vect[i-1]][vect[i]] = NULL;
+    adj[vect[i]][vect[i-1]] = NULL;
+  }
   if(DEBUG) printf("DAMN\n");
+  // Print output file
   fprintf(fpw, "%hd %hd %c %hd %d ", mp->y, mp->x, mp->mode, mp->nPoints, cost);
   printPoints(lt, fpw, &count, mp);
   fprintf(fpw, "\n");
-
+  // free all
   freeList(lt);
   freeAdj(adj, mp->nPoints);
 }
 
 
-
-// recursive function to find hamilton paths
+/**
+*  - hamAndCheese - Recursive function to find best hamilton path (taking cost into consideration)
+                    Each recursive call is a position in the intended path (from 0 to nPoints) where each vertice only appears once
+*
+* @param int pos - position in the vector that we are tying to find a vertice to (increments each recursive call)
+* @param map *mp - structure with map of cost, size of board and points of interest
+* @param list ***adj - matrix of pointers to the lists of the paths between those 2 points (adj[a][b] - best path bettween a and b represented as a list)
+* @param int vect[] - vector that stores the current path being examined (each position is a vertice)
+* @param int best[] - vector that stores the best path
+* @param int cost - stores the current cost of the path being examined
+* @param int bCost - stores the cost of the best path sored in best[]
+*
+**/
 void hamAndCheese(int pos, map *mp, list ***adj, int vect[], int best[], int cost, int *bCost) {
   int i, j;
 
+  // found a possible path
   if( pos == mp->nPoints) {
     if(DEBUG) printf("Encontrou um caminho\n");
+    // compare the cost with prevous path to find the best
     if (*bCost == 0 || cost < *bCost) {
       if(DEBUG) printf("O caminho e o melhor ate agora\n");
       for (i= 0; i<mp->nPoints; i++)
@@ -214,14 +247,17 @@ void hamAndCheese(int pos, map *mp, list ***adj, int vect[], int best[], int cos
     for (i = 0; i< pos; i++) printf(" %d |", vect[i]);
     printf("\n");
   }
+  // find next vertice to assign to vect[pos]
   for (i = 1; i< mp->nPoints; i++) {
+    // is it already in vect[]? and is it a possible path?
     if ( newAdj(vect, pos, i) && (adj[vect[pos-1]][i] != NULL || adj[i][vect[pos-1]] != NULL)) {
       if(DEBUG) printf("Tentar %d (custo %d)\n", i, ((node*)(adj[vect[pos-1]][i]->item))->cost );
       vect[pos] = i;
-      if (adj[vect[pos-1]][i] == NULL) {
+      if (adj[vect[pos-1]][i] == NULL) { // path needs to be reversed
         adj[vect[pos-1]][i] = reverseList(adj[i][vect[pos-1]], mp, i);
         adj[i][vect[pos-1]] = NULL;
       }
+      // call itself to finde next step (vect[pos+1])
       hamAndCheese(pos+1, mp, adj, vect, best, cost + ((node *)(adj[vect[pos-1]][i]->item))->cost, bCost);
       if(DEBUG) {
         printf("Regrediu\n");
@@ -232,11 +268,18 @@ void hamAndCheese(int pos, map *mp, list ***adj, int vect[], int best[], int cos
     }
     if(DEBUG) printf("%d- nao satifaz if\n", i);
   }
-
   return;
 }
 
-
+/**
+*  - newAdj - verify if vertice "i" is already in vect[]
+*
+* @param int vect[] - vector that stores the current path being examined (each position is a vertice)
+* @param int pos - position in the vector that we are tying to find a vertice to (increments each recursive call)
+* @param int i - vertice to be examined
+*
+* @return "true2 if it is new and "false" if it was already used
+**/
 bool newAdj(int vect[], int pos, int i) {
   int j;
   for (j = 1; j<pos; j++)
@@ -244,7 +287,14 @@ bool newAdj(int vect[], int pos, int i) {
   return true;
 }
 
-
+/**
+*  - reverseList - reverse the list order to be used backwards
+*
+* @param list *lt - list to be reversed
+* @param map *mp - structure with map of cost, size of board and points of interest
+* @param int pos - position of the first vertice (to be added since it is not in the list)
+*
+**/
 list *reverseList(list *lt, map *mp, int pos) {
   if (lt == NULL) return lt;
   int cost = ((node*)(lt->item))->cost - mp->map[((node*)(lt->item))->y][((node*)(lt->item))->x];
@@ -275,6 +325,13 @@ list *reverseList(list *lt, map *mp, int pos) {
 }
 
 
+/**
+*  - freeAdj - free matrix of lists
+*
+* @param list ***adj - matrix of pointers to the lists of the paths between those 2 points (adj[a][b] - best path bettween a and b represented as a list)
+* @param int n - number width and height of matrix
+*
+**/
 void freeAdj(list ***adj, int n) {
   int i, j;
 
@@ -287,12 +344,13 @@ void freeAdj(list ***adj, int n) {
   free(adj);
 }
 
-/*
-in: mp -- map of city
-    x,y -- coordinats to inMapCheck
-return: 1 if valid
-        0 if unvalid
-*/
+
+/**
+*in: mp -- map of city
+*    x,y -- coordinats to inMapCheck
+*return: 1 if valid
+*        0 if unvalid
+**/
 short int inMapCheck (map *mp, short int x, short int y){
   if (x >= 0 && x < mp->x && y >= 0 && y < mp->y && (mp->map == NULL || mp->map[y][x] != 0))
     return 1;
